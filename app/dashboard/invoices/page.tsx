3fa1acdfc404081cf9808ed0d0398cff2a1abc1e
c0ka <julianhy@gmail.com>
2026-01-08 12:04:29 +0800

    CH10 completed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -16,6 +16,7 @@ export default async function Page(prop: {
   const searchParams = await prop.searchParams;
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -30,7 +31,7 @@ export default async function Page(prop: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
